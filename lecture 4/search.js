2bdf04620129b637768c42b1a609260eb9efaaa9
juliettegodyere <juliettejuli@yahoo.com>
2021-03-11 17:36:24 +0100

    Commit lecture 4 code

---
+++ lecture 4/search.js
@@ -0,0 +1,15 @@
+
+//https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
+function search_animal() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('animals'); 
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="list-item";                  
+        } 
+    } 
+} 

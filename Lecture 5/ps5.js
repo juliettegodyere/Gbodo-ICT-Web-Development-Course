73901c1084b94f39f7fb0ff08e601e8ecf7d771d
juliettegodyere <juliettejuli@yahoo.com>
2021-03-18 19:38:19 +0100

    Commit Lecture 5 code

---
+++ Lecture 5/ps5.js
@@ -0,0 +1,7 @@
+var text = '{"vehicle":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+
+obj = JSON.parse(text);
+document.getElementById("demo").innerHTML = obj.employees[1].firstName + " " + obj.employees[1].lastName;

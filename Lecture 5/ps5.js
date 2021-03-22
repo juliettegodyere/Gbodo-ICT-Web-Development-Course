f2561c3960c92a60398a9e773a92f8299fcf5ff9
Babajide-ojo <ojobabajide629@gmail.com>
2021-03-22 02:24:59 +0100

    committed

--- Lecture 5/ps5.js
+++ Lecture 5/ps5.js
@@ -1,7 +1,16 @@
-var text = '{"vehicle":[' +
+function menuIcon() {
+    var x = document.getElementById('menu');
+    var y = document.getElementById('header');
+    document.getElementById('search').classList.toggle("hide");
+    x.classList.toggle("change");
+    y.classList.toggle("show");
+  }
+
+
+/**var text = '{"vehicle":[' +
 '{"firstName":"John","lastName":"Doe" },' +
 '{"firstName":"Anna","lastName":"Smith" },' +
 '{"firstName":"Peter","lastName":"Jones" }]}';
 
 obj = JSON.parse(text);
-document.getElementById("demo").innerHTML = obj.employees[1].firstName + " " + obj.employees[1].lastName;
+document.getElementById("demo").innerHTML = obj.employees[1].firstName + " " + obj.employees[1].lastName;**/

721364656093b59d67485ddff3a01c9ef86b0628
Babajide-ojo <ojobabajide629@gmail.com>
2021-04-02 10:14:25 +0100

    lecture 6 code

--- Lecture 6/ps6.js
+++ Lecture 6/ps6.js
@@ -11,17 +11,3 @@ function searchBar(){
 
 
 }
-
-
-var text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-
-var obj = JSON.parse(text);
-
-<p id="demo"></p>
-
-
-document.getElementById("demo").innerHTML =
-obj.employees[1].firstName + " " + obj.employees[1].lastName;

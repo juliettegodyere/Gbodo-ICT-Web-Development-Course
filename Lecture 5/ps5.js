fb945932aca27ab0491735f208d0ef892ef06db5
Victoria Kazeem <victoria.kazeem@pmglobaltechnology.com>
2021-04-09 09:09:30 +0100

    Modified the ReadMe file

--- Lecture 5/ps5.js
+++ Lecture 5/ps5.js
@@ -1,12 +1,12 @@
 function menuIcon() {
-    var x = document.getElementById('menu');
-    var y = document.getElementById('header');
-    document.getElementById('search').classList.toggle("hide");
-    x.classList.toggle("change");
-    y.classList.toggle("show");
-  }
+  var x = document.getElementById('menu');
+  var y = document.getElementById('header');
+  document.getElementById('search').classList.toggle("hide");
+  x.classList.toggle("change");
+  y.classList.toggle("show");
+}
 
-function searchBar(){
+function searchBar() {
   document.getElementById('search-bar').style.display = "block";
 
 
@@ -14,9 +14,9 @@ function searchBar(){
 
 
 var text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+  '{ "firstName":"John" , "lastName":"Doe" },' +
+  '{ "firstName":"Anna" , "lastName":"Smith" },' +
+  '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
 var obj = JSON.parse(text);
 
@@ -24,4 +24,4 @@ var obj = JSON.parse(text);
 
 
 document.getElementById("demo").innerHTML =
-obj.employees[1].firstName + " " + obj.employees[1].lastName;
+  obj.employees[1].firstName + " " + obj.employees[1].lastName;
